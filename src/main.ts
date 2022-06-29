6aaa09b96a5d59c60114af2f48cebfa977bf7199
Boris <toborto@yandex.ru>
2022-06-29 14:44:44 +0300

    Обновление проекта 11

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
+//import  showcase3  from "./plugins/showcase3";
 
 loadFonts();
 
 createApp(App).use(router).use(vuetify).mount("#app");
+
+//createApp(App).use(router).use(vuetify).use(showcase3).mount("#app");
---
+++ src/plugins/showcase3.ts
@@ -0,0 +1,3 @@
+//import { createShowcase3 } from "showcase3";
+
+//export default createShowcase3();
