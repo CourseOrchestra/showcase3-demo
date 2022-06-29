4e9412057c63011148d3d57dc3de4d493cf10129
Boris <toborto@yandex.ru>
2022-06-29 17:46:11 +0300

    Подключение cypress 3

--- tests/e2e/specs/test.cy.js
+++ tests/e2e/specs/test.cy.js
@@ -1,8 +1,32 @@
-// https://docs.cypress.io/api/table-of-contents
-
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("e2e тесты", () => {
+  it("Welcome", () => {
     cy.visit("/");
-    cy.contains("h1", "Vue");
+    cy.contains("span", "Welcome");
+    cy.contains("span", "Логотип КУРСа");
+    cy.contains("span", "Логотип Платформы");
+    cy.contains("span", "Навигатор");
+    cy.contains("span", "Грид");
+    cy.contains("span", "Состояние");
+  });
+  it("Логотип КУРСа", () => {
+    cy.visit("/logocurs");
+    cy.get("#lcurs1").should("have.attr", "href").and("contain", "curs.ru");
+  });
+  it("Логотип Платформы", () => {
+    cy.visit("/logoplatform");
+    cy.get("#lplatform1")
+      .should("have.attr", "href")
+      .and("contain", "corchestra.ru");
+  });
+  it("Навигатор", () => {
+    cy.visit("/navigator");
+    cy.contains("h1", "Демонстрация навигатора");
+  });
+  it("Грид", () => {
+    cy.visit("/grid");
+  });
+  it("Состояние в url", () => {
+    cy.visit("/urlstate");
+    cy.contains("h1", "Демонстрация состояния в url");
   });
 });
