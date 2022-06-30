efc218cc622fd9ebdb78325bcff83d3f5ad6a8f1
Boris <toborto@yandex.ru>
2022-06-30 12:39:38 +0300

    Подключение cypress 6

--- cypress/component/WelcomePage.cy.ts
+++ cypress/component/WelcomePage.cy.ts
@@ -1,5 +1,19 @@
+import WelcomePage from "@/components/WelcomePage.vue";
+
 describe("WelcomePage.cy.ts", () => {
   it("playground", () => {
-    //cy.mount()
+    /* eslint @typescript-eslint/ban-ts-comment: "off" */
+    // @ts-ignore
+    cy.mount(WelcomePage);
+    //    cy.contains("h1", "Vue");
+    cy.contains("Vue");
+
+    /*
+
+    cy.get('[data-testid=stepper]')
+        .should('be.visible')
+        .and('contain.text', '1');
+
+*/
   });
 });
