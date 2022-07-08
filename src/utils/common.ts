9cb0d29a6295d03c72edbdbdfb2c2a6297bc6300
Boris <toborto@yandex.ru>
2022-07-08 14:08:38 +0300

    Подключение vitest

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,4 +1,5 @@
-const MAIN_TITLE: string = process.env.VUE_APP_TITLE;
+//const MAIN_TITLE: string = process.env.VUE_APP_TITLE;
+const MAIN_TITLE = "Демо showcase3";
 export function setTitle(title: string) {
   document.title = title ? title + " - " + MAIN_TITLE : MAIN_TITLE;
 }
