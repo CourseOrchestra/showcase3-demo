632f4be7a7d82dc6ea30a4603e1f4eac99e077b8
Boris <toborto@yandex.ru>
2022-07-11 13:02:43 +0300

    Выпиливание @vue/cli 4

--- tests/e2e/specs/test.cy.ts
+++ tests/e2e/specs/test.cy.ts
@@ -1,6 +1,6 @@
 describe("e2e тесты", () => {
   it("Welcome", () => {
-    cy.visit("/");
+    cy.visit("http://localhost:3000/");
     cy.contains("div", "Welcome");
     cy.contains("div", "Логотип КУРСа");
     cy.contains("div", "Логотип Платформы");
@@ -9,24 +9,24 @@ describe("e2e тесты", () => {
     cy.contains("div", "Состояние");
   });
   it("Логотип КУРСа", () => {
-    cy.visit("/logocurs");
+    cy.visit("http://localhost:3000/logocurs");
     cy.get("#lcurs1").should("have.attr", "href").and("contain", "curs.ru");
   });
   it("Логотип Платформы", () => {
-    cy.visit("/logoplatform");
+    cy.visit("http://localhost:3000/logoplatform");
     cy.get("#lplatform1")
       .should("have.attr", "href")
       .and("contain", "corchestra.ru");
   });
   it("Навигатор", () => {
-    cy.visit("/navigator");
+    cy.visit("http://localhost:3000/navigator");
     cy.contains("h1", "Демонстрация навигатора");
   });
   it("Грид", () => {
-    cy.visit("/grid");
+    cy.visit("http://localhost:3000/grid");
   });
   it("Состояние в url", () => {
-    cy.visit("/urlstate");
+    cy.visit("http://localhost:3000/urlstate");
     cy.contains("h1", "Демонстрация состояния в url");
   });
 });
