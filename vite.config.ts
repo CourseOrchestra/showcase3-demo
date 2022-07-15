b1094f2d4a901d21490556ff77aa9cbbf72689e7
Boris <toborto@yandex.ru>
2022-07-15 13:54:09 +0300

    Переход на новую идеологию 27

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vuetify from "vite-plugin-vuetify";
 import { visualizer } from "rollup-plugin-visualizer";
+import { createHtmlPlugin } from "vite-plugin-html";
 
 /* eslint @typescript-eslint/no-var-requires: "off" */
 const path = require("path");
@@ -9,57 +10,54 @@ const path = require("path");
 const publicPath =
   process.env.NODE_ENV === "production" ? "/showcase3-demo/" : "/";
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    visualizer({
-      filename: "dist/stats.html",
-    }),
-
-    {
-      name: "vitest-plugin-beforeall",
-      config: () => ({
-        test: { setupFiles: ["./tests/unit/beforeall.ts"] },
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd());
+  return {
+    plugins: [
+      createHtmlPlugin({
+        minify: true,
+        inject: {
+          data: {
+            title: env.VITE_APP_TITLE,
+          },
+        },
       }),
+
+      visualizer({
+        filename: "dist/stats.html",
+      }),
+
+      {
+        name: "vitest-plugin-beforeall",
+        config: () => ({
+          test: { setupFiles: ["./tests/unit/beforeall.ts"] },
+        }),
+      },
+
+      vue(),
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
+      vuetify({
+        autoImport: true,
+      }),
+    ],
+    base: publicPath,
+    define: {
+      "process.env": {
+        BASE_URL: publicPath,
+      },
+    },
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+      },
     },
 
-    vue(),
-    // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vite-plugin
-    vuetify({
-      autoImport: true,
-    }),
-  ],
-  base: publicPath,
-  define: {
-    "process.env": {
-      BASE_URL: publicPath,
+    test: {
+      globals: true,
+      environment: "happy-dom",
+      deps: {
+        inline: ["vuetify"],
+      },
     },
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-
-  test: {
-    globals: true,
-    environment: "happy-dom",
-    deps: {
-      inline: ["vuetify"],
-    },
-  },
-
-  /* remove the need to specify .vue files https://vitejs.dev/config/#resolve-extensions
-  resolve: {
-    extensions: [
-      '.js',
-      '.json',
-      '.jsx',
-      '.mjs',
-      '.ts',
-      '.tsx',
-      '.vue',
-    ]
-  },
-  */
+  };
 });
