d706a9a5a341c7a135976656707c13415a9fdf13
Boris <toborto@yandex.ru>
2022-07-15 11:44:36 +0300

    Переход на новую идеологию 20

--- cypress/component/GridPage.cy.ts
+++ cypress/component/GridPage.cy.ts
@@ -3,8 +3,6 @@ import GridPage from "@/components/GridPage.vue";
 
 describe("GridPage.cy.ts", () => {
   it("Грид", () => {
-    /* eslint @typescript-eslint/ban-ts-comment: "off" */
-    // @ts-ignore
     cy.mount(GridPage, { extensions: { plugins: [vuetify] } });
     cy.contains("h1", "Демонстрация грида");
   });
--- cypress/component/WelcomePage.cy.ts
+++ cypress/component/WelcomePage.cy.ts
@@ -3,8 +3,6 @@ import WelcomePage from "@/components/WelcomePage.vue";
 
 describe("WelcomePage.cy.ts", () => {
   it("Welcome", () => {
-    /* eslint @typescript-eslint/ban-ts-comment: "off" */
-    // @ts-ignore
     cy.mount(WelcomePage, { extensions: { plugins: [vuetify] } });
     cy.contains("h1", "Vue");
     cy.contains("h1", "Vuetify");
