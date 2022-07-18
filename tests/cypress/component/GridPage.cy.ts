3678b28a92a3c1cbf9b48dd3d3c566f4af886eaa
Boris <toborto@yandex.ru>
2022-07-18 18:21:24 +0300

    Переход на новую идеологию 42

--- tests/cypress/component/GridPage.cy.ts
+++ tests/cypress/component/GridPage.cy.ts
@@ -2,8 +2,11 @@ import vuetify from "@/plugins/vuetify";
 import GridPage from "@/components/GridPage.vue";
 
 describe("GridPage.cy.ts", () => {
-  it("Грид", () => {
-    cy.mount(GridPage, { extensions: { plugins: [vuetify] } });
-    cy.contains("h1", "Демонстрация грида");
+  it("Грид", (done) => {
+    setTimeout(() => {
+      cy.mount(GridPage, { extensions: { plugins: [vuetify] } });
+      cy.contains("h1", "Демонстрация грида");
+      done();
+    }, 100);
   });
 });
--- tests/cypress/component/WelcomePage.cy.ts
+++ tests/cypress/component/WelcomePage.cy.ts
@@ -2,9 +2,12 @@ import vuetify from "@/plugins/vuetify";
 import WelcomePage from "@/components/WelcomePage.vue";
 
 describe("WelcomePage.cy.ts", () => {
-  it("Welcome", () => {
-    cy.mount(WelcomePage, { extensions: { plugins: [vuetify] } });
-    cy.contains("h1", "Vue");
-    cy.contains("h1", "Vuetify");
+  it("Welcome", (done) => {
+    setTimeout(() => {
+      cy.mount(WelcomePage, { extensions: { plugins: [vuetify] } });
+      cy.contains("h1", "Vue");
+      cy.contains("h1", "Vuetify");
+      done();
+    }, 100);
   });
 });
