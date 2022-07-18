1b00f4c86fafa29a763b0500c5638becef383ab4
Boris <toborto@yandex.ru>
2022-07-18 16:45:03 +0300

    Переход на новую идеологию 38

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,19 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
-  e2e: {},
+  fixturesFolder: "tests/cypress/fixtures",
+  screenshotsFolder: "tests/cypress/screenshots",
+  videosFolder: "tests/cypress/videos",
+
+  e2e: {
+    supportFile: "tests/cypress/support/e2e.ts",
+    specPattern: "tests/cypress/e2e/**/*.cy.{js,jsx,ts,tsx}",
+  },
 
   component: {
+    supportFile: "tests/cypress/support/component.ts",
+    specPattern: "tests/cypress/component/**/*.cy.{js,jsx,ts,tsx}",
+    indexHtmlFile: "tests/cypress/support/component-index.html",
     devServer: {
       framework: "vue",
       bundler: "vite",
