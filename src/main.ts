0b4a4ad04b565422535f2820328a03ce6b509ca9
Boris <toborto@yandex.ru>
2022-07-27 18:03:24 +0300

    Переход на новую идеологию 66

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import App from "./App.vue";
 import router from "./router";
 import vuetify from "./plugins/vuetify";
 import { loadFonts } from "./plugins/webfontloader";
-//import  showcase3  from "./plugins/showcase3";
+//import showcase3 from "showcase3";
 
 loadFonts();
 
-createApp(App).use(router).use(vuetify).mount("#app");
-
-//createApp(App).use(router).use(vuetify).use(showcase3).mount("#app");
+createApp(App)
+  .use(router)
+  .use(vuetify) /*.use(showcase3)*/
+  .mount("#app");
--- src/plugins/showcase3.ts
+++
@@ -1,3 +0,0 @@
-//import { createShowcase3 } from "showcase3";
-
-//export default createShowcase3();
