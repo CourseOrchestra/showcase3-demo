9b2868ef5875bcb782f500d7956c514d5acc459f
Boris <toborto@yandex.ru>
2022-10-26 13:09:32 +0300

    Переход на новую идеологию 122

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,8 +1,7 @@
 import { $tt } from "@/plugins/i18n";
 
-const MAIN_TITLE: string = import.meta.env.VITE_APP_TITLE;
-
 export function setTitle(title: string) {
+  const MAIN_TITLE = $tt("App.platform");
   title = $tt("App.menu." + title);
   document.title = title ? title + " - " + MAIN_TITLE : MAIN_TITLE;
 }
