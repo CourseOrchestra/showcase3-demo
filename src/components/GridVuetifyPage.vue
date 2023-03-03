25570f000a671cdc1ce0d83dec9dbb40978b9758
Boris <toborto@yandex.ru>
2023-03-03 12:03:25 +0300

    Переход на новую идеологию 161

--- src/components/GridVuetifyPage.vue
+++ src/components/GridVuetifyPage.vue
@@ -10,7 +10,7 @@
       :items="desserts"
       :items-length="totalDesserts"
       :loading="loading"
-      :items-per-page="5"
+      :items-per-page="10"
       item-value="name"
       show-select
       show-expand
@@ -172,6 +172,86 @@ export default {
         },
         {
           name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: "Frozen Yogurt 2",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: 1,
+        },
+        {
+          name: "Ice cream sandwich 2",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: 1,
+        },
+        {
+          name: "Eclair 2",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: 7,
+        },
+        {
+          name: "Cupcake 2",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: 8,
+        },
+        {
+          name: "Gingerbread 2",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: 16,
+        },
+        {
+          name: "Jelly bean 2",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: 0,
+        },
+        {
+          name: "Lollipop 2",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: 2,
+        },
+        {
+          name: "Honeycomb 2",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: "Donut 2",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: "KitKat 2",
           calories: 518,
           fat: 26.0,
           carbs: 65,
