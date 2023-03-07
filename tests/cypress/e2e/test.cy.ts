ded6ae6be0c4a63a62ed594d0ce5c4a44429c453
Boris <toborto@yandex.ru>
2023-03-07 10:33:02 +0300

    Переход на новую идеологию 168

--- tests/cypress/e2e/test.cy.ts
+++ tests/cypress/e2e/test.cy.ts
@@ -6,7 +6,7 @@ describe("e2e тесты", () => {
     cy.contains("div", "Логотип Платформы");
     cy.contains("div", "Навигатор");
     cy.contains("div", "Грид");
-    cy.contains("div", "Состояние");
+    //    cy.contains("div", "Состояние");
   });
   it("Логотип КУРСа", () => {
     cy.visit("/logocurs");
@@ -25,8 +25,11 @@ describe("e2e тесты", () => {
   it("Грид", () => {
     cy.visit("/grid");
   });
+
+  /*
   it("Состояние в url", () => {
     cy.visit("/urlstate");
     cy.contains("h1", "Демонстрация состояния в url");
   });
+*/
 });
