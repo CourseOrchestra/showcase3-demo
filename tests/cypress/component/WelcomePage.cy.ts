0c964662f5b4ac6c7274ce6969d17fe61584ca19
Boris <toborto@yandex.ru>
2023-07-17 11:19:55 +0300

    Переход на новую идеологию 304

--- tests/cypress/component/WelcomePage.cy.ts
+++ tests/cypress/component/WelcomePage.cy.ts
@@ -1,7 +1,7 @@
 import WelcomePage from "@/components/WelcomePage.vue";
 
 describe("WelcomePage.cy.ts", () => {
-  it.skip("Welcome", () => {
+  it("Welcome", () => {
     cy.mount(WelcomePage);
     cy.contains("h1", "Vue");
     cy.contains("h1", "Vuetify");
