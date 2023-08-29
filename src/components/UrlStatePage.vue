88e4e4797bb8d008636492952271474fbabd1652
Boris <toborto@yandex.ru>
2023-08-29 12:06:18 +0300

    Переход на новую идеологию 326

--- src/components/UrlStatePage.vue
+++ src/components/UrlStatePage.vue
@@ -1,94 +1,104 @@
 <template>
   <vContainer>
-    <!--    tag::template_i18n[] -->
-    <h1>{{ $t("UrlStatePage.title") }}</h1>
-    <!--    end::template_i18n[] -->
+    <v-app-bar color="teal-darken-4">
+      <template #image>
+        <v-img
+          gradient="to top right, rgba(19,84,122,.8), rgba(128,208,199,.8)"
+        ></v-img>
+      </template>
+      <v-btn @click="changeViolationName">change Violation Name</v-btn>
+      <v-btn @click="changeStrName">change Str Name</v-btn>
+      <!--    tag::template_i18n[] -->
+      <v-app-bar-title>{{ $t("UrlStatePage.title") }} </v-app-bar-title>
+      <!--    end::template_i18n[] -->
+    </v-app-bar>
 
-    <div class="home">
-      <table>
-        <tr>
-          <td>Field stored to the local storage22:</td>
-          <td><input v-model="query.page" /><br /><br /></td>
-        </tr>
+    <br />
+    <br />
+    <vRow>
+      <vCol>
+        <vTextField
+          v-model="violation.group"
+          outlined
+          label="violation.group"
+        ></vTextField>
+      </vCol>
+      <vCol>
+        <vTextField
+          v-model="violation.name"
+          outlined
+          label="violation.name"
+        ></vTextField>
+      </vCol>
+    </vRow>
+    <vRow>
+      <vCol>
+        <vTextField
+          v-model="violation.act"
+          outlined
+          label="violation.act"
+        ></vTextField>
+      </vCol>
+      <vCol>
+        <vTextField
+          v-model="violation.inspector"
+          outlined
+          label="violation.inspector"
+        ></vTextField>
+      </vCol>
+    </vRow>
+    <vRow>
+      <vCol>
+        <vTextField
+          v-model="violation.status"
+          outlined
+          label="violation.status"
+        ></vTextField>
+      </vCol>
+      <vCol>
+        <vTextField v-model="str" outlined label="str"></vTextField>
+      </vCol>
+    </vRow>
 
-        <tr>
-          <td><input v-model="str" /><br /><br /></td>
+    <vRow>
+      <vCol>
+        <vTextField
+          v-model="query.page"
+          outlined
+          label="query.page"
+        ></vTextField>
+      </vCol>
+      <vCol>
+        <vTextField
+          v-model="query.page22"
+          outlined
+          label="query.page22"
+        ></vTextField>
+      </vCol>
+    </vRow>
+    <vRow>
+      <vCol>
+        <vTextField v-model="query.num" outlined label="query.num"></vTextField>
+      </vCol>
+      <vCol>
+        Add and remove (the query param is called dynarr):
+        <input v-model="tmp" placeholder="Enter value" />
+        <button @click="query.addValue('dynarr', tmp)">Add</button>&nbsp;
+        <button @click="query.removeValue('dynarr', tmp)">Remove</button>
+      </vCol>
+    </vRow>
 
-          <td>
-            <button @click="func22">func</button>
-          </td>
-        </tr>
+    <v-btn @click="convert"
+      >Convert simple text field to numeric model (can be used to, for example,
+      define datatype later)</v-btn
+    >
 
-        <tr>
-          <td>Simple text field 22</td>
-          <td><input v-model="query.page22" /><br /><br /></td>
-        </tr>
-        <tr>
-          <td>Number field with the default value of 10:</td>
-          <td><input v-model="query.num" type="number" /><br /><br /></td>
-        </tr>
-        <tr>
-          <td>Checkbox:</td>
-          <td><input v-model="query.check" type="checkbox" /><br /><br /></td>
-        </tr>
-        <tr>
-          <td>
-            Array (multiple selection, control-click for selecting both values):
-          </td>
-          <td>
-            <select v-model="query.option" multiple>
-              <option value="first">First choice</option>
-              <option value="second">Second choice</option></select
-            ><br /><br />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            Comma-separated array (multiple selection, control-click for
-            selecting both values):
-          </td>
-          <td>
-            <select v-model="query.option2" multiple>
-              <option value="first">First choice</option>
-              <option value="second">Second choice</option></select
-            ><br /><br />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            Space-separated array (multiple selection, control-click for
-            selecting both values):
-          </td>
-          <td>
-            <select v-model="query.option3" multiple>
-              <option value="first">First choice</option>
-              <option value="second">Second choice</option></select
-            ><br /><br />
-          </td>
-        </tr>
-        <tr>
-          <td>Add and remove (the query param is called dynarr):</td>
-          <td>
-            <input v-model="tmp" placeholder="Enter value" /><br /><br />
-            <button @click="query.addValue('dynarr', tmp)">Add</button>&nbsp;
-            <button @click="query.removeValue('dynarr', tmp)">Remove</button>
-            <br />
-          </td>
-        </tr>
-      </table>
-      <br /><br />
-      <button @click="convert">
-        Convert simple text field to numeric model (can be used to, for example,
-        define datatype later)
-      </button>
-      <br /><br />
-      query equals:
-      <pre>{{ query }}</pre>
-      stringified query:
-      <pre>{{ JSON.stringify(query, null, 4) }}</pre>
-      definition (not reactive):
-      <pre>{{ query.__definition }}</pre>
-    </div>
+    <br /><br />
+    query equals:
+    <pre>{{ query }}</pre>
+    <br />
+    definition (not reactive):
+    <pre>{{ query.__definition }}</pre>
   </vContainer>
 </template>
 
@@ -101,50 +111,27 @@ setTitle("urlstate");
 
 const query = useQuery();
 
-/*
-
-UrlMapper.run(myModel, router);
-
-
-UrlMapper.run(router, myModel.fio, myModel.dateFrom);
-
-
-UrlMapper.run(router, {   {myModel.fio.value}, "fio", "string"} );
-
-
-UrlMapper.addParam(router, {   {myModel.fio.value}, "fio", "string"} );
-
-
-query.parentId = myModel.parentId;
-
-*/
-
-//console.log(query);
-
-//    query.parentId = myModel.parentId;
-
-/*
-    const str = ref();
-    query.str = str;
-*/
-//    const str = query.str;
-
+const violation = ref({});
 const str = ref();
-if (query.str) {
-  str.value = query.str;
-}
-query.str = str;
-//console.log(str);
 
-function func22() {
-  str.value = "gg";
+//query.define("violation", CommaArrayDatatype, []);
+
+query.violation = violation;
+
+query.str = str;
+
+function changeViolationName() {
+  violation.value.name = "ff";
   //console.log(str);
+}
+function changeStrName() {
+  str.value = "gg";
 }
 
 query.define("dynarr", CommaArrayDatatype, []);
 const tmp = ref("");
 
 function convert() {
-  query.define("page", IntDatatype, 0);
+  query.define("page", IntDatatype, 10);
 }
 </script>
