4072bb5f7f898deecefd9d862610be82004150bf
Boris <toborto@yandex.ru>
2023-09-04 16:44:11 +0300

    Переход на новую идеологию 357

--- src/library/index.ts
+++ src/library/index.ts
@@ -1,43 +1,34 @@
 import { reactive, watch } from "vue";
 import { queryFingerprint } from "./utils";
 import { LocationQuery, Router } from "vue-router";
-import { DataType, NavigationOperation, ParsedQuery } from "./types";
+import { DetailedFingerprint, ParsedQuery } from "./types";
 import { WatchStopHandle } from "vue";
 
 const _query = reactive({
   query: {} as ParsedQuery,
   rawQuery: {} as LocationQuery,
-  serializedId: 0,
 });
 
 let router!: Router;
-let debug = false;
-let navigationOperation!: (
-  query: ParsedQuery,
-  router: Router,
-) => "push" | "replace";
 
 let fingerprint: string | null = null;
-//let detailedFingerprint: DetailedFingerprint = {};
+let detailedFingerprint: DetailedFingerprint = {};
 let watchers: { [key: string]: WatchStopHandle } = {};
 
 /* eslint @typescript-eslint/no-explicit-any: "off" */
-function dlog(...args: any[]) {
-  if (debug) {
-    // eslint-disable-next-line no-console
-    console.log(...args);
-  }
-}
-
 function serializeChangedValue(key: string, value: any) {
   _query.rawQuery[key] = JSON.stringify(value, null);
-  _query.serializedId++;
+
+  const actualFingerprint = queryFingerprint(_query.rawQuery);
+  fingerprint = actualFingerprint.fingerprint;
+  detailedFingerprint = actualFingerprint.detailedFingerprint;
+
+  router["replace"]({ query: _query.rawQuery });
 }
 function setWatcher(key: string) {
   if (key in watchers) {
     return;
   }
-  dlog("Adding watcher", key);
 
   watchers[key] = watch(
     () => _query.query[key],
@@ -52,6 +43,8 @@ function clearWatchers() {
 }
 
 function handleRouteChange() {
+  fingerprint = null;
+  detailedFingerprint = {};
   Object.keys(_query.query).forEach(function (key) {
     delete _query.query[key];
   });
@@ -59,9 +52,14 @@ function handleRouteChange() {
 }
 
 function parseAndStoreQuery(
-  query: LocationQuery /*,  actualDetailedFingerprint: DetailedFingerprint,*/,
+  query: LocationQuery,
+  actualDetailedFingerprint: DetailedFingerprint,
 ) {
   for (const key of Object.keys(query)) {
+    if (detailedFingerprint[key] === actualDetailedFingerprint[key]) {
+      continue;
+    }
+
     if (query[key]) {
       _query.query[key] = JSON.parse(query[key]!.toString());
     }
@@ -71,57 +69,24 @@ function parseAndStoreQuery(
   Object.assign(_query.rawQuery, query);
 }
 
-function setup(
-  _router: Router,
-  _debug: boolean,
-  _navigationOperation: NavigationOperation,
-) {
+function setup(_router: Router) {
   router = _router;
-  debug = _debug;
-  if (_navigationOperation === "push" || _navigationOperation === "replace") {
-    navigationOperation = () => _navigationOperation;
-  } else {
-    navigationOperation = _navigationOperation;
-  }
 
   router.beforeEach((to, from) => {
-    dlog("beforeEach called", to.name, from.name);
     if (to.name !== from.name) {
       handleRouteChange();
     }
-    // check fingerprint if query modified
+
     const actualFingerprint = queryFingerprint(to.query);
     if (actualFingerprint.fingerprint === fingerprint) {
-      dlog("same fingerprint, not changing args");
       return;
     }
-    dlog("parsing args", actualFingerprint);
 
-    // parse and store query params
-    parseAndStoreQuery(to.query /*, actualFingerprint.detailedFingerprint*/);
+    parseAndStoreQuery(to.query, actualFingerprint.detailedFingerprint);
 
-    // set up watchers
-    for (const key of Object.keys(_query.query)) {
-      setWatcher(key);
-    }
-
-    // set fingerprint
     fingerprint = actualFingerprint.fingerprint;
-    //detailedFingerprint = actualFingerprint.detailedFingerprint;
+    detailedFingerprint = actualFingerprint.detailedFingerprint;
   });
-
-  watch(
-    () => _query.serializedId,
-    () => {
-      // no need to go through the parsing process again when router calls beforeEach,
-      // so set the fingerprint
-      const actualFingerprint = queryFingerprint(_query.rawQuery);
-      fingerprint = actualFingerprint.fingerprint;
-      //detailedFingerprint = actualFingerprint.detailedFingerprint;
-      const op = navigationOperation(_query.query, router);
-      router[op]({ query: _query.rawQuery });
-    },
-  );
 }
 
 export type URLObject = {
@@ -161,16 +126,11 @@ const QuerySynchronizer = {
     app: any,
     {
       router,
-      debug,
-      navigationOperation,
     }: {
       router: Router;
-      datatypes?: Array<DataType<any>>;
-      debug?: boolean;
-      navigationOperation?: NavigationOperation;
     },
   ) {
-    setup(router, debug || false, navigationOperation || "push");
+    setup(router);
   },
 };
 export default QuerySynchronizer;
