e98b594fa1e4e590c2903d2766c1236ac356d2b3
Boris <toborto@yandex.ru>
2023-11-14 12:12:36 +0300

    Переход на новую идеологию 390

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -2,8 +2,6 @@ import "@mdi/font/css/materialdesignicons.css";
 import "vuetify/styles";
 
 import { createVuetify } from "vuetify";
-import { VDataTableServer } from "vuetify/labs/VDataTable";
-import { VDataTableVirtual } from "vuetify/labs/VDataTable";
 
 import { createVueI18nAdapter } from "vuetify/locale/adapters/vue-i18n";
 import { useI18n } from "vue-i18n";
@@ -12,10 +10,6 @@ import { i18n } from "./i18n";
 const vuetify = createVuetify({
   locale: {
     adapter: createVueI18nAdapter({ i18n, useI18n }),
-  },
-  components: {
-    VDataTableServer,
-    VDataTableVirtual,
   },
 });
 
