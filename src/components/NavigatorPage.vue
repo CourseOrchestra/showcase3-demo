4dba50dde47c489df8e322d3e902b2b571572808
Boris <toborto@yandex.ru>
2023-12-27 10:54:41 +0300

    Переход на новую идеологию 398

--- src/components/NavigatorPage.vue
+++ src/components/NavigatorPage.vue
@@ -2,14 +2,57 @@
   <vContainer>
     <br />
     <br />
+    <!--    <h1 class="text-center">{{ $t("NavigatorPage.title") }}</h1>-->
+    <h1 class="text-center">Демонстрация компонента c-overflow-text</h1>
     <br />
     <br />
-    <h1 class="text-center">{{ $t("NavigatorPage.title") }}</h1>
+    <br />
+    <br />
+
+    <v-card width="300" title="Заголовок" variant="outlined">
+      <c-overflow-text
+        :text="
+          '1111 2222 3333 4444 5555 6666 7777 ' +
+          '9999999999999999999999999999999999  00000000000000' +
+          '1111111111 3333333333333 454444444 6666666666 88888 999999 2222222' +
+          'dasda fdgdg fghfghfghfg fghfghgf'
+        "
+        :line-clamp="3"
+      >
+      </c-overflow-text>
+    </v-card>
+
+    <!--
+    <v-table>
+      <thead>
+      <tr>
+        <th class="text-left">
+
+          <c-overflow-text
+              :text="'1111 2222 3333 4444 5555 6666 7777 9999999999999999999999999999999999  00000000000000'"
+          >
+          </c-overflow-text>
+
+        </th>
+        <th class="text-left">
+          Calories
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>gggg</td>
+        <td>ffff</td>
+      </tr>
+      </tbody>
+    </v-table>
+-->
   </vContainer>
 </template>
 
 <script setup lang="ts">
 import { setTitle } from "@/utils/common";
+import COverflowText from "@/components/common/COverflowText.vue";
 
 setTitle("navigator");
 </script>
---
+++ src/components/common/COverflowText.vue
@@ -0,0 +1,31 @@
+<template>
+  <div
+    class="c-overflow-text"
+    :title="props.text"
+    :style="`-webkit-line-clamp: ${props.lineClamp}`"
+  >
+    {{ props.text }}
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  lineClamp: {
+    type: Number,
+    default: 4,
+  },
+});
+</script>
+
+<style scoped>
+.c-overflow-text {
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+}
+</style>
