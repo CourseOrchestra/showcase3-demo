40fab1a81b717b3e968a5cdc96e09796a732501d
Boris <toborto@yandex.ru>
2024-01-17 11:10:38 +0300

    Переход на новую идеологию 403

--- src/components/NavigatorPage.vue
+++ src/components/NavigatorPage.vue
@@ -1,11 +1,7 @@
 <template>
   <vContainer>
-    <br />
-    <br />
     <!--    <h1 class="text-center">{{ $t("NavigatorPage.title") }}</h1>-->
     <h1 class="text-center">Демонстрация компонента c-overflow-text</h1>
-    <br />
-    <br />
     <br />
     <br />
 
@@ -21,6 +17,9 @@
       >
       </c-overflow-text>
     </v-card>
+
+    <br />
+    <br />
 
     <!--
     <v-table>
@@ -47,12 +46,273 @@
       </tbody>
     </v-table>
 -->
+
+    <v-data-table-server
+      :headers="headers"
+      :items="desserts"
+      :items-length="totalDesserts"
+      :loading="loading"
+      :items-per-page="10"
+      item-value="name"
+      show-select
+      show-expand
+      class="elevation-1"
+      fixed-footer
+      fixed-header
+      @update:options="options = $event"
+    >
+      <template #expanded-row> This is an expanded row </template>
+    </v-data-table-server>
   </vContainer>
 </template>
 
-<script setup lang="ts">
+<script>
 import { setTitle } from "@/utils/common";
 import COverflowText from "@/components/common/COverflowText.vue";
 
-setTitle("navigator");
+export default {
+  components: {
+    COverflowText,
+  },
+
+  data() {
+    setTitle("navigator");
+
+    return {
+      totalDesserts: 0,
+      desserts: [],
+      loading: true,
+      options: {},
+      headers: [
+        {
+          title: "Dessert (100g serving)",
+          align: "start",
+          sortable: false,
+          key: "name",
+        },
+        { title: "Calories", key: "calories" },
+        { title: "Fat (g)", key: "fat" },
+        { title: "Carbs (g)", key: "carbs" },
+        { title: "Protein (g)", key: "protein" },
+        { title: "Iron (%)", key: "iron" },
+      ],
+    };
+  },
+  watch: {
+    options: {
+      handler() {
+        this.getDataFromApi();
+      },
+      deep: true,
+    },
+  },
+  methods: {
+    getDataFromApi() {
+      this.loading = true;
+      this.fakeApiCall().then((data) => {
+        this.desserts = data.items;
+        this.totalDesserts = data.total;
+        this.loading = false;
+      });
+    },
+    /**
+     * In a real application this would be a call to fetch() or axios.get()
+     */
+    fakeApiCall() {
+      return new Promise((resolve /*, reject*/) => {
+        const { sortBy = [], page, itemsPerPage } = this.options;
+        let items = this.getDesserts();
+        const total = items.length;
+        if (sortBy.length) {
+          items = items.sort((a, b) => {
+            const sortA = a[sortBy[0].key];
+            const sortB = b[sortBy[0].key];
+            if (sortBy[0].order) {
+              if (sortA < sortB) return 1;
+              if (sortA > sortB) return -1;
+              return 0;
+            } else {
+              if (sortA < sortB) return -1;
+              if (sortA > sortB) return 1;
+              return 0;
+            }
+          });
+        }
+        if (itemsPerPage > 0) {
+          items = items.slice((page - 1) * itemsPerPage, page * itemsPerPage);
+        }
+        setTimeout(() => {
+          resolve({
+            items,
+            total,
+          });
+        }, 200);
+      });
+    },
+    getDesserts() {
+      return [
+        {
+          name: "Frozen Yogurt",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: 1,
+        },
+        {
+          name: "Ice cream sandwich",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: 1,
+        },
+        {
+          name: "Eclair",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: 7,
+        },
+        {
+          name: "Cupcake",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: 8,
+        },
+        {
+          name: "Gingerbread",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: 16,
+        },
+        {
+          name: "Jelly bean",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: 0,
+        },
+        {
+          name: "Lollipop",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: 2,
+        },
+        {
+          name: "Honeycomb",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: "Donut",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: "KitKat",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+        {
+          name: "Frozen Yogurt 2",
+          calories: 159,
+          fat: 6.0,
+          carbs: 24,
+          protein: 4.0,
+          iron: 1,
+        },
+        {
+          name: "Ice cream sandwich 2",
+          calories: 237,
+          fat: 9.0,
+          carbs: 37,
+          protein: 4.3,
+          iron: 1,
+        },
+        {
+          name: "Eclair 2",
+          calories: 262,
+          fat: 16.0,
+          carbs: 23,
+          protein: 6.0,
+          iron: 7,
+        },
+        {
+          name: "Cupcake 2",
+          calories: 305,
+          fat: 3.7,
+          carbs: 67,
+          protein: 4.3,
+          iron: 8,
+        },
+        {
+          name: "Gingerbread 2",
+          calories: 356,
+          fat: 16.0,
+          carbs: 49,
+          protein: 3.9,
+          iron: 16,
+        },
+        {
+          name: "Jelly bean 2",
+          calories: 375,
+          fat: 0.0,
+          carbs: 94,
+          protein: 0.0,
+          iron: 0,
+        },
+        {
+          name: "Lollipop 2",
+          calories: 392,
+          fat: 0.2,
+          carbs: 98,
+          protein: 0,
+          iron: 2,
+        },
+        {
+          name: "Honeycomb 2",
+          calories: 408,
+          fat: 3.2,
+          carbs: 87,
+          protein: 6.5,
+          iron: 45,
+        },
+        {
+          name: "Donut 2",
+          calories: 452,
+          fat: 25.0,
+          carbs: 51,
+          protein: 4.9,
+          iron: 22,
+        },
+        {
+          name: "KitKat 2",
+          calories: 518,
+          fat: 26.0,
+          carbs: 65,
+          protein: 7,
+          iron: 6,
+        },
+      ];
+    },
+  },
+};
 </script>
