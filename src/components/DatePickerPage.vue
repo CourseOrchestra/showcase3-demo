0ecf1d67ae4b2b979aa000fc3cecc7ec064b2327
Boris <toborto@yandex.ru>
2024-02-29 17:53:32 +0300

    Переход на новую идеологию 442

--- src/components/DatePickerPage.vue
+++ src/components/DatePickerPage.vue
@@ -5,8 +5,6 @@
     <br />
     <br />
 
-    <!--    <v-date-picker v-model="arrdate" multiple="2"></v-date-picker>-->
-
     <c-range-date-picker
       v-model="dateRange"
       label="range date-picker"
@@ -46,50 +44,6 @@
     </c-btn>
     <br />
     <br />
-
-    <h2>Собственно, календарь</h2>
-    <div class="card flex">
-      <Calendar v-model="date" date-format="dd/mm/yy" />
-    </div>
-
-    <br />
-    <h2>Дополнительные способы показа календаря</h2>
-    <br />
-    <div class="card flex flex-wrap gap-3 p-fluid">
-      <div class="flex-auto">
-        <label for="buttondisplay" class="font-bold block mb-2">
-          По кнопке
-        </label>
-        <Calendar v-model="buttondisplay" show-icon input-id="buttondisplay" />
-      </div>
-      <div class="flex-auto">
-        <label for="icondisplay" class="font-bold block mb-2">
-          По картинке
-        </label>
-        <Calendar
-          v-model="icondisplay"
-          show-icon
-          icon-display="input"
-          input-id="icondisplay"
-        />
-      </div>
-      <div class="flex-auto">
-        <label for="templatedisplay" class="font-bold block mb-2">
-          По шаблону картинки
-        </label>
-        <Calendar
-          v-model="templatedisplay"
-          show-icon
-          icon-display="input"
-          time-only
-          input-id="templatedisplay"
-        >
-          <template #inputicon="{ clickCallback }">
-            <i class="pi pi-clock" @click="clickCallback" />
-          </template>
-        </Calendar>
-      </div>
-    </div>
   </vContainer>
 </template>
 
@@ -103,26 +57,19 @@ import CRangeDatePickerOld from "@/components/common/CRangeDatePickerOld.vue";
 
 setTitle("datepicker");
 
-const date = ref();
-
-const buttondisplay = ref();
-const icondisplay = ref();
-const templatedisplay = ref();
-
-date.value = new Date();
-buttondisplay.value = new Date();
-icondisplay.value = new Date();
-
 const dateField = ref();
 
 dateField.value = "2024-02-07";
 
-//const dateRange =  new SrokDTO();
-
 /*
+
+const date = ref();
+date.value = new Date();
+
+const dateRange =  new SrokDTO();
+
 const dateStart2 =   ref("2024-02-07");
 const dateEnd2 =   ref("2024-02-07");
-
 
 const dateRange = ref({
   dateStart: dateStart2,
@@ -145,9 +92,4 @@ const debugDate2 = () => {
   /* eslint-disable-next-line  no-console */
   console.log(dateRange.value);
 };
-
-/*
-const arrdate = ref();
-arrdate.value = [new Date("2024-02-07"), new Date("2024-02-09")];
-*/
 </script>
