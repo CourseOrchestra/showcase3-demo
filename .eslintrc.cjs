b65e0001589711c3d5a118addfe093e18c2e7daa
Boris <toborto@yandex.ru>
2024-06-20 16:58:31 +0300

    Переход на новую идеологию 541

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,7 +6,7 @@ module.exports = {
   extends: [
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
-    "@vue/typescript/recommended",
+    "@vue/eslint-config-typescript",
     "plugin:prettier/recommended",
   ],
   overrides: [
